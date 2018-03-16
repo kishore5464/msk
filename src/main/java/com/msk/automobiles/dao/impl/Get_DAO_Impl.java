fa51d2af115675281492d9ce1800d3cbb0149a8f
kicha <kishorekumar5464@gmail.com>
2018-03-16 18:26:13 +0530

    done with brand and model

--- src/main/java/com/msk/automobiles/dao/impl/Get_DAO_Impl.java
+++ src/main/java/com/msk/automobiles/dao/impl/Get_DAO_Impl.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Repository;
 
 import com.msk.automobiles.dao.interfaces.Get_DAO_Interface;
 import com.msk.automobiles.service.entities.Car_Brands;
+import com.msk.automobiles.service.entities.Car_Models;
+import com.msk.automobiles.service.pojos.UICar_Models;
 
 @Repository
 @SuppressWarnings("unchecked")
@@ -30,6 +32,19 @@ public class Get_DAO_Impl implements Get_DAO_Interface {
 		session.flush();
 		session.close();
 		return car_Brands;
+	}
+
+	@Override
+	public List<Car_Models> getModels(Integer car_brands_id) {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		Query query = session.createQuery(" from Car_Models where car_brands_id = '"+car_brands_id+"'");
+		List<Car_Models> car_Models = query.list();
+		transaction.commit();
+		session.flush();
+		session.close();
+		return car_Models;
 	}
 
 }
