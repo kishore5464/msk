ba0ad484e0741bc2ce04860bca53fb790e18e367
sornaraj <sornarajct@gmail.com>
2018-03-22 17:50:21 +0530

    dfdsf

--- src/main/webapp/js/spareparts.js
+++ src/main/webapp/js/spareparts.js
@@ -7,7 +7,6 @@ $.ajax({
         datatype: 'text',
         success: function(response) {
 
-        	console.log("RESPONSE===========>"+response);
         	
           var output = JSON.parse(response);
 
@@ -29,5 +28,96 @@ $.ajax({
           console.log("Error AJax")
         }
       })
+      
+      
+      
+      $(document).on("change",".brandlist",function(){
+					  		  
+							  var selectedbrand=$(this).val();
+							  
+							  console.log("selectedbrand======>"+selectedbrand);
+	
+							  $('#car_modals')
+							    .find('option')
+							    .remove()
+							    .end()
+							    .append('<option value="" selected disabled>Select Modal</option>')
+							    .val('whatever');
+			  
+							$.ajax({
+						          url: 'msk/parts-models',
+						          type: 'POST',
+						          dataType: 'text',
+						          data: {'brand_id':selectedbrand},
+						          success: function(responseText) {
+						        	  
+						        	  console.log("output========>"+responseText);
+						        	  
+						        	  var output=JSON.parse(responseText);
+						        	  
+						        	
+						          	             
+						           $.each(output.data.models, function(key, value) {   
+						            	 
+						       		  $('#car_modals').append($("<option></option>").attr("value",key).val(value.model_id).text(value.model));
+						       		 
+						       		
+						            
+						          }); 
+						           $('.modallist').selectpicker('refresh');
+						      
+						          },
+						          error: function() {
+						          	console.log("error ajax");
+						          	
+						          }
+						      });
+						  
+						  });
+
+
+
+$(document).on("change",".modallist",function(){
+	  
+	  var selectedbrand=$(this).val();
+	  
+	  console.log("selectedbrand======>"+selectedbrand);
+
+
+	$.ajax({
+        url: 'exists-partss',
+        type: 'POST',
+        dataType: 'text',
+        data: {'brand_id':selectedbrand},
+        success: function(responseText) {
+      	  
+      	  console.log("output========>"+responseText);
+      	  
+      	  var output=JSON.parse(responseText);
+      	  
+      	
+      	 var availableTags = [];
+	        
+         $.each(output.nameeeeeee, function(key, value) {
+           
+             availableTags.push(value.nameeeeeee);
+             
+            
+           });
+         
+        
+         $( ".parts_name" ).autocomplete({
+             source: availableTags
+           });
+         
+    
+        },
+        error: function() {
+        	console.log("error ajax");
+        	
+        }
+    });
+
+});
       
      
