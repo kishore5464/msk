9643a85e21933c7ce07aa9028f244da3c8f664c1
kicha <kishorekumar5464@gmail.com>
2018-03-28 10:33:05 +0530

    ODne

---
+++ src/main/java/com/msk/automobiles/service/controller/TestController.java
@@ -0,0 +1,61 @@
+package com.msk.automobiles.service.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.server.mvc.Viewable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Controller;
+
+import com.msk.automobiles.business.interfaces.Get_Business_Interface;
+import com.msk.automobiles.business.interfaces.Insert_Business_Interface;
+import com.msk.automobiles.business.interfaces.Update_Business_Interface;
+import com.msk.automobiles.exception.CustomGenericException;
+import com.msk.automobiles.service.pojos.Car_Brands_Pojo;
+import com.msk.automobiles.service.pojos.Car_Models_Pojo;
+import com.msk.automobiles.service.pojos.Customer_Details_Pojo;
+import com.msk.automobiles.service.pojos.Location_Pojo;
+import com.msk.automobiles.service.pojos.Service_Type_Pojo;
+import com.msk.automobiles.service.pojos.Spare_Parts_Pojo;
+
+import net.minidev.json.JSONObject;
+
+@Configuration
+@PropertySource("classpath:/application_path.properties")
+@Controller
+@Path("/")
+public class TestController {
+
+	@Autowired
+	Get_Business_Interface get_Business_Interface;
+
+	@Autowired
+	Insert_Business_Interface insert_Business_Interface;
+
+	@Autowired
+	Update_Business_Interface update_Business_Interface;
+
+	@GET
+	@Path("/service-card")
+	public Response car_brand() {
+		Viewable view = null;
+		try {
+			view = new Viewable("/service_card");
+
+		} catch (Exception e) {
+			throw new CustomGenericException("" + e.hashCode(), e.getMessage());
+		}
+
+		return Response.ok().entity(view).build();
+	}
+
+}
